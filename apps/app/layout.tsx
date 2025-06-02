18edd19a61320015ab7f0674ebe6e38265d5710d
Hao Ly <kevinly0609@gmail.com>
2025-06-02 12:38:43 -0700

    Updated dashboard page UI

--- apps/app/layout.tsx
+++ apps/app/layout.tsx
@@ -15,8 +15,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <Navigation />
-        {children}
+        <div className='container'>
+          <Navigation />
+          {children}
+        </div>
       </body>
     </html>
   );
--- apps/components/navigation/Navigation.tsx
+++ apps/components/navigation/Navigation.tsx
@@ -1,18 +1,30 @@
 "use client";
 
 import Image from "next/image";
-import logo from '@/public/Logo.jpg';
+import logo from "@/public/Logo.jpg";
 import { JSX } from "react";
+import styles from "./Navigation.module.css";
+import Link from "next/link";
 
 const Navigation: React.FC = (): JSX.Element => {
-    return (
-        <Image 
-            alt="Logo"
-            src={logo}
-            width={150}
-            height={150}
+  return (
+    <nav className={styles.navigation_bar}>
+      <Link href='/'>
+        <Image
+          alt="Logo"
+          src={logo}
+          className={styles.navigation_logo}
         />
-    );
+      </Link>
+      <div className={styles.navigation_components}>
+        <Link href='/' className={styles.navigation_components_part}>Behind AlgoSync</Link>
+        <Link href='/' className={styles.navigation_components_part}>Sync In</Link>
+        <Link href='/' className={styles.navigation_journey_button}>
+          <button>Get Started</button>
+        </Link>
+      </div>
+    </nav>
+  );
 };
 
 export default Navigation;
