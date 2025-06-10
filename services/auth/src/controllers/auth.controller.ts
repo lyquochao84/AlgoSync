9d5c7bdba11869a2776d7ab093e8cd1470dd6432
Hao Ly <kevinly0609@gmail.com>
2025-06-10 14:30:03 -0700

    Added Prisma to auth-service

---
+++ services/auth/src/controllers/auth.controller.ts
@@ -0,0 +1,17 @@
+import { PrismaClient } from "@prisma/client";
+// import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const prisma = new PrismaClient();
+
+// Register
+export const register = async () => {
+
+};
+
+// Sync-In
+export const login = async () => {
+
+};
+
+
